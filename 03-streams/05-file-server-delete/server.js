d82aa56911f99c4628e1f6dfca7b8c42231c9804
Nikolay Trotskyi <nikmetal91@gmail.com>
2022-01-23 21:18:07 +0200

    add solution for 3-module 5-task

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,8 +11,30 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  // Exclude dirnames
+  if ( pathname.includes( '/' ) ) {
+    res.statusCode = 400;
+    res.end( 'Wrong filename' );
+  }
+
   switch (req.method) {
     case 'DELETE':
+      fs.unlink( filepath, function( err ) {
+        let message;
+
+        if ( err && err.code === 'ENOENT' ) {
+          res.statusCode = 404;
+          message = 'File doesn\'t exist';
+        } else if ( err ) {
+          res.statusCode = 500;
+          message = 'Something went wrong';
+        } else {
+          res.statusCode = 200;
+          message = 'File successfully removed';
+        }
+
+        res.end( message );
+      } );
 
       break;
 
